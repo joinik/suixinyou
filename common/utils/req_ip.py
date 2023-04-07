05b0051b8de6e4c20c09c834a3fab83ab0b0c7ac
jk <52019869+joinik@users.noreply.github.com>
2023-04-07 12:30:09 +0800

    Update req_ip.py
    更新key

--- common/utils/req_ip.py
+++ common/utils/req_ip.py
@@ -4,7 +4,7 @@ import httpx
 def req_area(ip):
     #  通过ip 获取 地理位置
 
-    resp = httpx.get('https://restapi.amap.com/v5/ip?key=677f025efeda715a72a7837f85f576f9&type=4&ip={}'.format(ip))
+    resp = httpx.get('https://restapi.amap.com/v5/ip?key={}&type=4&ip={}'.format(key, ip))
     assert resp.status_code == 200
     resp = resp.json()
     city = resp.get('city', '宿迁市')
